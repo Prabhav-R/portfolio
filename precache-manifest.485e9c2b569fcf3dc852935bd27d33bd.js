43d257b2991baff3c24dc8b5f9b4b774242e5b2b
Prabhav-R <prabhavrajeev.51@gmail.com>
2020-04-03 20:38:04 +0530

    Updates

---
+++ precache-manifest.485e9c2b569fcf3dc852935bd27d33bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24979fb291e64998e62762a7aefc1209",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c3dd51a32aeb83cbd7c4",
+    "url": "/portfolio/static/js/2.feca967a.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/portfolio/static/js/2.feca967a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789e6f7af5f49fe8600d",
+    "url": "/portfolio/static/js/main.0c8d641e.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
